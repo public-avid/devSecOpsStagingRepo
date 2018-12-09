c4861df72aeb292b856e4be1b1f75cc75f788f41
Siddharth Kundal <siddharthkundal@Siddharths-MacBook-Pro.local>
2018-12-10 00:06:28 +0530

    Sdhgb

--- demoCT.tf
+++ demoCT.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_instance" "exampsle" {
+resource "aws_instance" "example" {
   ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
 
