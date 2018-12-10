0b9bb2a30bab54c4b91b4eb8ae3511fb3f1d2000
Siddharth Kundal <siddharthkundal@Siddharths-MacBook-Pro.local>
2018-12-10 15:13:01 +0530

    S;duhgs

--- same.tf
+++ same.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region     = "${var.region}"
 }
 
-resource "aws_instance" "exss" {
+resource "aws_instance" "ex" {
   ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
 
