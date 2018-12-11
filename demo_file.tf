42ff96d72701b8fea5ad5885c3be409de9c7efed
Siddharth Kundal <siddharthkundal@Siddharths-MacBook-Pro.local>
2018-12-12 03:51:41 +0530

    sugfvdb

--- demo_file.tf
+++ demo_file.tf
@@ -182,12 +182,12 @@ resource "aws_redshift_cluster" "default" {
 }
 
 resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 16
-  require_lowercase_characters   = true
-  require_numbers                = true
-  require_uppercase_characters   = true
-  require_symbols                = true
-  allow_users_to_change_password = true
+  minimum_password_length        = "${var.minimum_password_length}"
+  require_lowercase_characters   = "${var.require_lowercase_characters}"
+  require_numbers                = "${var.require_numbers}"
+  require_uppercase_characters   = "${var.require_uppercase_characters}"
+  require_symbols                = "${var.require_symbols}"
+  allow_users_to_change_password = "${var.allow_users_to_change_password}"
 }
 
 resource "aws_iam_account_password_policy" "strict_2" {
